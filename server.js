06190f5b0780a5e3969ac3fb8c550553caa3c72e
jean <jeandzoko@gmail.com>
2025-10-01 21:33:22 +0000

    Third commit

--- server.js
+++ server.js
@@ -10,10 +10,13 @@ import connectDB from "./db.js";
 connectDB()
 dotenv.config();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://frontend-three-zeta-20.vercel.app/"
+    ]
   })
 );
 app.use(express.json());
